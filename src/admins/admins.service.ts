28c790b122f019c947c175f63662edabeeff0aa5
hojiakbar.2201 <hojiakbar.2201@gmail.com>
2025-06-09 09:27:40 +0500

    last last version

--- src/admins/admins.service.ts
+++ src/admins/admins.service.ts
@@ -27,12 +27,32 @@ export class AdminsService {
     return this.prismaService.admin.findUnique({ where: { id } });
   }
 
-  update(id: number, updateAdminDto: UpdateAdmninDto) {
+  async update(id: number, updateAdminDto: UpdateAdmninDto) {
+  const { password, confirm_password, ...values } = updateAdminDto;
+
+  // Faqat ikkala parol ham mavjud bo‘lsa
+  if (password !== undefined || confirm_password !== undefined) {
+    if (!password || !confirm_password) {
+      throw new BadRequestException("Ikkala parol ham birga berilishi kerak");
+    }
+
+    if (password !== confirm_password) {
+      throw new BadRequestException("Parollar mos emas");
+    }
+
+    const hashed_password = await bcrypt.hash(password, 7);
     return this.prismaService.admin.update({
       where: { id },
-      data: updateAdminDto,
+      data: { ...values, hashed_password },
     });
   }
+
+  // Parol yo‘q bo‘lsa, oddiy update
+  return this.prismaService.admin.update({
+    where: { id },
+    data: values,
+  });
+}
 
   remove(id: number) {
     return this.prismaService.admin.delete({where:{id}});
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -44,12 +44,33 @@ export class UsersService {
     return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+ async update(id: number, updateUserDto: UpdateUserDto) {
+  const { password, confirm_password, ...values } = updateUserDto;
+
+  // Faqat ikkala parol ham mavjud bo‘lsa
+  if (password !== undefined || confirm_password !== undefined) {
+    if (!password || !confirm_password) {
+      throw new BadRequestException("Ikkala parol ham birga berilishi kerak");
+    }
+
+    if (password !== confirm_password) {
+      throw new BadRequestException("Parollar mos emas");
+    }
+
+    const hashed_password = await bcrypt.hash(password, 7);
     return this.prismaService.user.update({
       where: { id },
-      data: updateUserDto,
+      data: { ...values, hashed_password },
     });
   }
+
+  // Parol yo‘q bo‘lsa, oddiy update
+  return this.prismaService.user.update({
+    where: { id },
+    data: values,
+  });
+}
+
 
   remove(id: number) {
     return this.prismaService.user.delete({ where: { id } });
